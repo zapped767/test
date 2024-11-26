d9ec45584b2dbd86895a8f0e577e1c3110c4eac9
zapped767 <pathmanadhanarigaran16@gmail.com>
2024-11-26 13:17:26 +0530

    second commit

--- RoomReservationSystem/src/main/java/com/example/RoomReservationSystem/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.RoomReservationSystem.Repository;
-
-import com.example.RoomReservationSystem.Entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-}
